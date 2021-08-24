ed278690bf5420753e15fa139425196e80c6468a
talisilva <talissaagtha@gmail.com>
2021-08-24 14:21:58 -0300

    Blog Pessoal Aula 02

---
+++ src/main/java/org/generation/blogpessoal/controller/PostagemController.java
@@ -0,0 +1,15 @@
+package org.generation.blogpessoal.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+@RequestMapping("/postagens") 
+@CrossOrigin(origins = "*", allowedHeaders = "*") 
+public class PostagemController {
+	
+	
+}
