e62cd3ecdd96da5a86b7d4c3e0271438e753d865
talisilva <talissaagtha@gmail.com>
2021-08-25 10:34:50 -0300

    BlogPessoal Aula 03

--- src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
+++ src/main/java/org/generation/blogpessoal/repository/PostagemRepository.java
@@ -1,5 +1,7 @@
 package org.generation.blogpessoal.repository;
 
+import java.util.List;
+
 import org.generation.blogpessoal.model.Postagem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -8,6 +10,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface PostagemRepository extends JpaRepository<Postagem, Long> {
 
+	public List <Postagem> findAllByTituloContainingIgnoreCase(String titulo);
 	
 
 }
