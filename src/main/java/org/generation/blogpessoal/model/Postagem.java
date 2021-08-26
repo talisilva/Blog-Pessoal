fa55141d216af7ee30f7e3366806ebfc22030610
talisilva <talissaagtha@gmail.com>
2021-08-26 14:26:13 -0300

    Aula 04

--- src/main/java/org/generation/blogpessoal/model/Postagem.java
+++ src/main/java/org/generation/blogpessoal/model/Postagem.java
@@ -2,15 +2,18 @@ package org.generation.blogpessoal.model;
 
 	import java.util.Date;
 
-	import javax.persistence.Entity;
-	import javax.persistence.GeneratedValue;
-	import javax.persistence.GenerationType;
-	import javax.persistence.Id;
-	import javax.persistence.Table;
-	import javax.persistence.Temporal;
-	import javax.persistence.TemporalType;
-	import javax.validation.constraints.NotNull;
-	import javax.validation.constraints.Size;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
 	@Entity 
 	@Table(name = "tb_postagens") 
@@ -63,6 +66,10 @@ package org.generation.blogpessoal.model;
 		public void setData(Date data) {
 			this.data = data;
 		}
+		
+		@ManyToOne
+		@JsonIgnoreProperties("postagem")
+		private Tema tema;
 
 	}
 
